f4177e3e25e9b7f87dbec065512db618596dc8f9
Ashhad Sameer <ashhadsdevr@gmail.com>
2017-06-13 21:05:39 +0530

    pagination not working

--- blog/resources/views/admin_template.blade.php
+++ blog/resources/views/admin_template.blade.php
@@ -155,20 +155,20 @@
 
         <!-- jQuery 2.1.3 -->
         <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-        <!-- DataTables -->
-        <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
         <!-- Bootstrap 3.3.2 JS -->
         <script src="{{ asset ("/bower_components/admin-lte/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
+        <!-- DataTables -->
+        <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+        <!-- Bootstrap Datatables -->
+        <!-- <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js" type="text/javascript"></script> -->
         <!-- AdminLTE App -->
         <script src="{{ asset ("/bower_components/admin-lte/dist/js/app.min.js") }}" type="text/javascript"></script>
 
-        <!-- Bootstrap Datatables -->
-        <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js" type="text/javascript"></script>
 
         <!-- Date Picker -->
         <script src="/bower_components/admin-lte/plugins/datepicker/bootstrap-datepicker.js"></script>
         
-        <!-- Custom Tables -->
+        <!-- Custom Scripts -->
         <script src="{{ asset ("/js/custom.js") }}"></script>
 
         @stack('scripts')
