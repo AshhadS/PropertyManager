6caf45a4af9883131ac388879db7d5d1d6091bee
Irshad <shadapiit@gmail.com>
2017-08-04 14:32:15 +0530

    Expiring Agreements
    Expiring Agreements widget in the Dashboard

---
+++ blog/app/Model/DashboardController.php
@@ -0,0 +1,74 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\Unit;
+use App\Model\Property;
+use App\Model\RentalOwner;
+use App\Model\Tenant;
+use App\Model\JobCard;
+use App\Model\JobCardStatus;
+use App\Model\ExpiringAgreemntsOneMonth;
+use App\Model\ExpiringAgreemntsTwoMonth;
+use App\Model\ExpiringAgreemntsThreeMonth;
+use App\Model\Agreement;
+use App\Model\PaymentType;
+use Debugbar;
+use Datatables;
+use Sentinel;
+use Redirect;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends Controller
+{
+
+	function index(){
+		$propCount = Property::count();
+		$propOwnerCount = RentalOwner::count();
+		$unitCount = Unit::count();
+		$tenantsCount = Tenant::count();
+		$jobcardsCount = JobCard::count();
+
+		$units = Unit::all();
+    	$properties = Property::all();
+    	$tenants = Tenant::all();
+        $paymentypes = PaymentType::all();
+        $agreements = Agreement::all();
+
+		$jobCardStatusAll = JobCardStatus::all();
+        $jobCardStatusCount = array();
+
+        //Expire Agreements
+        $ExpiringAgreemntsTwoMonthCount=ExpiringAgreemntsTwoMonth::count();
+        $ExpiringAgreemntsThreeMonthCount=ExpiringAgreemntsThreeMonth::count();
+        $ExpiringAgreemntsOneMonth = ExpiringAgreemntsOneMonth::all();
+
+        foreach ($jobCardStatusAll as $status) {
+            $count = JobCard::where('jobcardStatusID', $status->jobcardStatusID)->count();
+            $jobCardStatusCount[$status->jobcardStatusID] = $count;
+        }
+
+		return view('dashboard', [
+	        'propCount' => $propCount,
+	        'propOwnerCount' => $propOwnerCount,
+	        'unitCount' => $unitCount,
+	        'tenantsCount' => $tenantsCount,
+	        'jobcardsCount' => $jobcardsCount,
+	        'jobCardStatusCount' => $jobCardStatusCount,
+	        'ExpiringAgreemntsThreeMonthCount' => $ExpiringAgreemntsThreeMonthCount,
+	        'ExpiringAgreemntsTwoMonthCount' => $ExpiringAgreemntsTwoMonthCount,
+	        'ExpiringAgreemntsOneMonth' => $ExpiringAgreemntsOneMonth,
+	        'units' => $units,
+	        'properties' => $properties,
+	        'tenants' => $tenants,
+	        'paymentypes' => $paymentypes,
+	        'agreements' => $agreements,
+	    ]);
+	}
+
+	 function getFields($agreementid){
+        $agreement = Agreement::where('agreementID', $agreementid)->get();
+        return $agreement;
+    }
+}
---
+++ blog/app/Model/ExpiringAgreemntsThreeMonth.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExpiringAgreemntsThreeMonth extends Model
+{
+    protected $table = 'agreement_expires_three_month';
+    
+}
