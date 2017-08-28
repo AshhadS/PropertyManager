d09c19d463ee12c9ebbc8195cf322ea99ed34663
Irshad <shadapiit@gmail.com>
2017-08-28 09:57:20 +0530

    Reports Route

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -128,7 +128,14 @@ Route::group(['middleware' => ['CustomAuth']], function () {
 	Route::post('/rentalowners/all', 'RentalOwnersController@data');
 
 
-
+		/**
+	 * Reports
+	 */
+	Route::get('/reports',  'ReportsController@index');
+	Route::get('/reports_supplierstatement',  'ReportsController@getSupplierStatement');
+	Route::get('/supplierstatement-print',  'ReportsController@printSupplierStatement');
+	Route::get('/supplierstatement-excel', 'ReportsController@excelSupplierStatement');
+	Route::get('/supplierStatement_pdf', 'ReportsController@pdfSupplierStatement');
 
 
 	/**
